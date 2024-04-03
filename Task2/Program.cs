76a794cd5c690515cd75185d42ca1f0cc6a6dcb3
EICH <thedimond22@mail.ru>
2024-04-03 15:11:16 +0300

    Add quotient Lesson1:Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -17,3 +17,9 @@ double product3 = d1 * i1;
 Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
+
+int quotient = i1 / i2;
+Console.WriteLine(quotient);
+
+double quotient2 = d1/i2;
+Console.WriteLine(quotient2);
--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+575409aa3b3e4bbb3c5f6e561d5d52e7090a68d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adf1c85307b5a254da6b818653e2be0d55e6887d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
