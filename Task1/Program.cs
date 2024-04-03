575409aa3b3e4bbb3c5f6e561d5d52e7090a68d3
EICH <thedimond22@mail.ru>
2024-04-03 14:24:56 +0300

    Add some variables Lesson1:Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,12 @@
 ﻿Console.WriteLine("Hello world");
+
+
+int value = 300;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
